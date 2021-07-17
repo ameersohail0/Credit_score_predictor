e7c507fd6efe143a7747c9b2a3a2cef19a5f5d61
Ameer Sohail <ameersohail0@gmail.com>
2021-07-18 03:33:05 +0530

    Added AML model to ml_utils

--- main.py
+++ main.py
@@ -15,57 +15,57 @@ app.add_event_handler("startup", load_model)
 
 # class which is expected in the payload
 class QueryIn(BaseModel):
-    Status_of_existing_account: int
-    Duration_of_Credit_month: int
-    Payment_Status_of_Previous_Credit: int
-    Purpose_of_loan: int
-    Credit_Amount:int
-    Value_of_Savings_accountbonds:int
-    Years_of_Present_Employment:int
-    Percentage_of_disposable_income:int
-    Sex_Marital_Status:int
-    Guarantors_Debtors:int
-    Duration_in_Present_Residence:int
-    Property:int
-    Age_in_years:int
-    Concurrent_Credits:int
-    Housing:int
-    No_of_Credits_at_this__Bank:int
-    Occupation:int
-    No_of_dependents:int
-    Telephone:int
-    Foreign_Worker:int
+    status: int
+    duration: int
+    credit_history: int
+    purpose: int
+    amount:int
+    savings:int
+    employment_duration:int
+    installment_rate:int
+    personal_status_sex:int
+    other_debtors:int
+    present_residence:int
+    property:int
+    age:int
+    other_installment_plans:int
+    housing:int
+    number_credits:int
+    job:int
+    people_liable:int
+    telephone:int
+    foreign_worker:int
 
 
 # class which is returned in the response
 class QueryOut(BaseModel):
-    Cost_Matrix_Risk: str
+    credit_risk: str
     #adding the timestamp as an attribute in the output model
     run_Time_timestamp : str
 
 # class which is expected in the payload while re-training
 class FeedbackIn(BaseModel):
-    Status_of_existing_account: int
-    Duration_of_Credit_month: int
-    Payment_Status_of_Previous_Credit: int
-    Purpose_of_loan: int
-    Credit_Amount:int
-    Value_of_Savings_accountbonds:int
-    Years_of_Present_Employment:int
-    Percentage_of_disposable_income:int
-    Sex_Marital_Status:int
-    Guarantors_Debtors:int
-    Duration_in_Present_Residence:int
-    Property:int
-    Age_in_years:int
-    Concurrent_Credits:int
-    Housing:int
-    No_of_Credits_at_this__Bank:int
-    Occupation:int
-    No_of_dependents:int
-    Telephone:int
-    Foreign_Worker:int
-    Cost_Matrix_Risk: str
+    status: int
+    duration: int
+    credit_history: int
+    purpose: int
+    amount:int
+    savings:int
+    employment_duration:int
+    installment_rate:int
+    personal_status_sex:int
+    other_debtors:int
+    present_residence:int
+    property:int
+    age:int
+    other_installment_plans:int
+    housing:int
+    number_credits:int
+    job:int
+    people_liable:int
+    telephone:int
+    foreign_worker:int
+    credit_risk: int
 
 
 # Route definitions
@@ -77,21 +77,27 @@ def ping():
 @app.get("/classes")
 # Healthcheck route to ensure that the API is up and running
 def classes():
-    return {'Status_of_existing_checking_account':{1:"no checking account",2:"<0 DM", 3: "0<= ... < 200 DM",4:"... >= 200 DM / salary for at least 1 year"},
-        'Payment_Status_of_Previous_Credit':{0:"delay in paying off in the past",1 : "critical account/other credits elsewhere",2:"no credits taken/all credits paid back duly",3:"existing credits paid back duly till now",4:"all credits at this bank paid back duly"},
-        'Purpose_of_loan':{0 : "others", 1 : "car (new)", 2 : "car (used)", 3 :"furniture/equipment" , 4 : "radio/television", 5 : "domestic appliances", 6 : "repairs", 7 : 'education',8 : 'vacation',9 : 'retraining',10 : 'business'},
-        'Value_of_Savings_accountbonds':{1 : "unknown/no savings account",2 :"... <  100 DM",3 : "100 <= ... <  500 DM",4 :"500 <= ... < 1000 DM", 5 :"... >= 1000 DM"},
-        'Years_of_Present_Employment':{5:">=7 years", 4:"4<= <7 years",  3:"1<= < 4 years", 2:"<1 years",1:"unemployed"},
-        'Percentage_of_disposable_income':{1 :" >= 35 ",2 : "25 <= ... < 35",3 :" 20 <= ... < 25",4 :" < 20" },
-        'Personal_status_and_sex':{ 1:"male : divorced/separated",2:"female : non-single or male : single",3:"male : married/widowed", 4:"female : single"},
-        'Guarantors_Debtors':{1:"none", 2:"co-applicant", 3:"guarantor"},
-        'Property':{4:"real estate", 3:"building soc. savings agr./life insurance", 2:"car or other", 1:"unknown / no property"},
-        'Concurrent_Credits':{3:"none", 2:"stores", 1:"bank"},
-        'Housing':{1:"for free", 3:"own", 2:"rent"},
-        'Occupation':{4:"management/ highly qualified employee", 3:"skilled employee / official", 2:"unskilled - resident", 1:"unemployed/ unskilled  - non-resident"},
-        'Telephone':{2:"yes", 1:"none"},
-        'foreign_worker':{1:"yes", 2:"no"},
-        'Cost_Matrix_Risk':{1:"Good Risk", 0:"Bad Risk"},
+    return {
+        'status':{1:"no checking account",2:"<0 DM", 3: "0<= ... < 200 DM",4:"... >= 200 DM / salary for at least 1 year"},
+        'duration': "duration of credit month",
+        'credit_history':{0:"delay in paying off in the past",1 : "critical account/other credits elsewhere",2:"no credits taken/all credits paid back duly",3:"existing credits paid back duly till now",4:"all credits at this bank paid back duly"},
+        'purpose':{0 : "others", 1 : "car (new)", 2 : "car (used)", 3 :"furniture/equipment" , 4 : "radio/television", 5 : "domestic appliances", 6 : "repairs", 7 : 'education',8 : 'vacation',9 : 'retraining',10 : 'business'},
+        'amount' : "credit amount in euros",
+        'savings':{1 : "unknown/no savings account",2 :"... <  100 DM",3 : "100 <= ... <  500 DM",4 :"500 <= ... < 1000 DM", 5 :"... >= 1000 DM"},
+        'employment_duration':{5:">=7 years", 4:"4<= <7 years",  3:"1<= < 4 years", 2:"<1 years",1:"unemployed"},
+        'installment_rate':{1 :" >= 35 ",2 : "25 <= ... < 35",3 :" 20 <= ... < 25",4 :" < 20" },
+        'personal_status_sex':{ 1:"male : divorced/separated",2:"female : non-single or male : single",3:"male : married/widowed", 4:"female : single"},
+        'other_debtors':{1:"none", 2:"co-applicant", 3:"guarantor"},
+        'present_residence' : "Duration in present residence",
+        'property':{4:"real estate", 3:"building soc. savings agr./life insurance", 2:"car or other", 1:"unknown / no property"},
+        'age' : "age in years",
+        'other_installment_plans':{3:"none", 2:"stores", 1:"bank"},
+        'housing':{1:"for free", 3:"own", 2:"rent"},
+        'number_credits' : "number of credits at this bank",
+        'job':{4:"management/ highly qualified employee", 3:"skilled employee / official", 2:"unskilled - resident", 1:"unemployed/ unskilled  - non-resident"},
+        'people_liable' : "number of dependents",
+        'telephone':{2:"yes", 1:"none"},
+        'foreign_worker':{1:"yes", 2:"no"}
             }
 
 
@@ -100,7 +106,7 @@ def classes():
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
 def predict_creditscore(query_data: QueryIn):
-    return {"Cost_Matrix_Risk": predict(query_data), "run_Time_timestamp":str(datetime.datetime.now().replace(microsecond=0))}
+    return {"credit_risk": predict(query_data), "run_Time_timestamp":str(datetime.datetime.now().replace(microsecond=0))}
 
 @app.post("/feedback_loop", status_code=200)
 # Route to further train the model based on user input in form of feedback loop
@@ -109,7 +115,6 @@ def predict_creditscore(query_data: QueryIn):
 def feedback_loop(data: List[FeedbackIn]):
     retrain(data)
     return {"detail": "Feedback loop successful", "run_Time_timestamp":str(datetime.datetime.now().replace(microsecond=0))}
-
 
 # Main function to start the app when main.py is called
 if __name__ == "__main__":
--- ml_utils.py
+++ ml_utils.py
@@ -1,47 +1,69 @@
 import numpy as np
 import pandas as pd
-import copy
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
+
+import h2o
+from h2o.automl import H2OAutoML
+
+h2o.init()
 
 #Using the South German Credit (UPDATE) Data Set
 #Dataset Link : https://archive.ics.uci.edu/ml/datasets/South+German+Credit+%28UPDATE%29
 
-# define a RandomForestClassifier classifier
-clf = RandomForestClassifier(n_estimators = 60, max_depth = 12, random_state = 7)
+dataset = "./SouthGermanCredit/SouthGermanCredit.csv"
+df = h2o.import_file(dataset)
+
+# Reponse column
+y = "credit_risk"
+
+# Split into train & test
+splits = df.split_frame(ratios = [0.8], seed = 1)
+train = splits[0]
+test = splits[1]
+
+# Run AutoML for 1 minute
+aml = None
+
+# # define a RandomForestClassifier classifier
+# clf = RandomForestClassifier(n_estimators = 60, max_depth = 12, random_state = 7)
 
 # define the class encodings and reverse encodings
 classes = {0: "Bad Risk", 1: "Good Risk"}
-r_classes = {y: x for x, y in classes.items()}
+risk_threshold = 0.50
 
 # function to train and load the model during startup
 def load_model():
-    # load the dataset from the official sklearn datasets
-    #X, y = datasets.load_iris(return_X_y=True)
-    df = pd.read_csv("SouthGermanCredit/SouthGermanCredit.asc",skiprows=4,encoding="gbk",engine='python',sep=' ',delimiter=None, index_col=False,header=None,skipinitialspace=True)
-    last_ix = len(df.columns) - 1
-    X, y = df.drop(last_ix, axis=1), df[last_ix]
-    # do the test-train split and train the model
-    #Splitting the data for training and testing
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2) #80% and 20%
-    clf.fit(X_train, y_train)
-
-    # calculate and the print the accuracy score
-    acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+    global aml
+    aml = H2OAutoML(max_runtime_secs=60, seed=1)
+    train_model()
+    
+def train_model():
+    aml.train(y=y, training_frame=train)
+    print("Auto ML Model Training Complete")
 
 # function to predict the Credit score using the model
 def predict(query_data):
     x = list(query_data.dict().values())
-    prediction = clf.predict([x])[0]
-    print(f"Model prediction: {classes[prediction]}")
-    return classes[prediction]
+    columns = list(query_data.dict().keys())
+    x = h2o.H2OFrame(x).transpose()
+    x.columns = columns
+    prediction = aml.leader.predict(x)[0,0]
+    if(prediction > risk_threshold):
+        print(f"Model prediction: {classes[1]}")
+        return classes[1]
+    else:
+        print(f"Model prediction: {classes[0]}")
+        return classes[0]
 
 # function to retrain the model as part of the feedback loop
 def retrain(data):
     # pull out the relevant X and y from the FeedbackIn object
-    X = [list(d.dict().values())[:-1] for d in data]
-    y = [r_classes[d.Cost_Matrix_Risk] for d in data]
-    # fit the classifier again based on the new data obtained
-    clf.fit(X, y)
+    global train
+    for ds in data:
+        x = list(ds.dict().values())
+        columns = list(ds.dict().keys())
+        x = h2o.H2OFrame(x).transpose()
+        x.columns = columns
+        train += x
+    print("Added data to the training set")
+    print("Retraining the Model")
+    train_model()
